5be41c4101477352a65104ae1a6b6c475dffa764
Mfivez <fivezmauritcioprogmail.com>
2024-10-28 10:04:20 +0100

    update

--- src/app/features/exercices/interceptor/components/correction/tools/auth.interceptor.ts
+++ src/app/features/exercices/interceptor/components/correction/tools/auth.interceptor.ts
@@ -1,14 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, finalize } from 'rxjs/operators';
-import { Router } from '@angular/router';
-import { LoaderService } from './loader.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
-  constructor(private router: Router, private loaderService: LoaderService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
       return throwError(() => "error"); 
